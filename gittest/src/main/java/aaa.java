528d1bfb2bbffcb75ec8c960324be1604917abe1
zhengtao <544024994@qq.com>
2019-03-12 16:15:43 +0800

    System.out.println("hello world3");

--- gittest/src/main/java/aaa.java
+++ gittest/src/main/java/aaa.java
@@ -2,5 +2,6 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
+        System.out.println("hello world3");
     }
 }
